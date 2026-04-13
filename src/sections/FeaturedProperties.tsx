13ffc8fe401fd368350e0b48b0d289e974da7448
Teshome-worku <teshomeworku96@gmail.com>
2026-04-13 17:05:45 +0300

    feat: add new properties and implement show/hide functionality in FeaturedProperties

--- src/sections/FeaturedProperties.tsx
+++ src/sections/FeaturedProperties.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { useState } from 'react';
 import PropertyCard from "@/components/PropertyCard";
 import { properties } from "@/data/properties";
 
 
 export default function FeaturedProperties() {
+    const [showAll, setShowAll] = useState(false);
+    const displayedProperties = showAll ? properties : properties.slice(0, 6);
     return (
         <section className="py-20 bg-gray-50">
             <div className="max-w-7xl mx-auto px-6">
@@ -20,14 +23,17 @@ export default function FeaturedProperties() {
                         </p>
                     </div>
 
-                    <button className="text-blue-600 font-medium hover:underline cursor-pointer">
-                        View All →
+                    <button
+                        className="text-blue-600 font-medium hover:underline cursor-pointer"
+                        onClick={() => setShowAll(!showAll)}
+                    >
+                        {showAll ? "Show Less ←" : "View All →"}
                     </button>
                 </div>
 
                 {/* GRID */}
                 <div className="grid md:grid-cols-3 gap-6">
-                    {properties.map((property, index) => (
+                    {displayedProperties.map((property, index) => (
                         <PropertyCard key={index} {...property} />
                     ))}
                 </div>
