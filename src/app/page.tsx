010765153cf4d1083e9246778cf232eb711abe78
Teshome-worku <teshomeworku96@gmail.com>
2026-04-13 14:25:52 +0300

    feat: add PropertyCard component to display featured properties

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import Hero from "@/sections/hero";
+import PropertyCard from "@/components/PropertyCard";
+
+const properties = [
+  {
+    image: "https://images.unsplash.com/photo-1568605114967-8130f3a36994",
+    price: "$120,000",
+    title: "Modern Family House",
+    location: "Addis Ababa, Bole",
+    beds: 3,
+    baths: 2,
+  },
+  {
+    image: "https://images.unsplash.com/photo-1600585154340-be6161a56a0c",
+    price: "$95,000",
+    title: "Luxury Villa",
+    location: "Addis Ababa, CMC",
+    beds: 4,
+    baths: 3,
+  },
+];
+
 export default function Home() {
   return (
-    <main >
+    <main>
       <Hero />
-
+      <PropertyCard properties={properties} />
     </main>
   );
 }
